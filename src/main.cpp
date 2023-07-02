eaf3bf6db6b24064dba8686d783deca516ab584d
Aiden Vigue <aiden@vigue.me>
2023-07-02 13:00:38 -0400

    bump version to 4.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,7 @@
 const char* systemType = "Tranquil";
 
 // System version
-const char* systemVersion = "4.0.2";
+const char* systemVersion = "4.1.0";
 
 // Build date
 const char* buildDate = __DATE__;
