d87c017f3e0d6ab69192d3b9d89f54eb3d8e0dcc
Aiden Vigue <aiden@vigue.me>
2023-07-23 23:38:03 -0400

    Fix RobotConfig json path error

--- src/RobotConfigurations.h
+++ src/RobotConfigurations.h
@@ -16,7 +16,7 @@ class RobotConfigurations
         Log.notice("RobotConfigurations: Requesting %s, there are %d default types\n", robotTypeName, _numRobotConfigurations);
         for (int configIdx = 0; configIdx < _numRobotConfigurations; configIdx++)
         {
-            String robotType = RdJson::getString("robotType", "", _robotConfigs[configIdx]);
+            String robotType = RdJson::getString("robotConfig/robotType", "", _robotConfigs[configIdx]);
             Log.verbose("RobotConfigurations: Testing %s against %s\n", robotType.c_str(), robotTypeName);
             if (robotType.equals(robotTypeName))
             {
@@ -32,7 +32,7 @@ class RobotConfigurations
         retStr = "[";
         for (int configIdx = 0; configIdx < _numRobotConfigurations; configIdx++)
         {
-            String robotType = RdJson::getString("robotType", "", _robotConfigs[configIdx]);
+            String robotType = RdJson::getString("robotConfig/robotType", "", _robotConfigs[configIdx]);
             if (configIdx != 0)
                 retStr += ",";
             retStr += "\"" + robotType + "\"";
@@ -45,6 +45,6 @@ class RobotConfigurations
     {
         if ((n < 0) || (n > _numRobotConfigurations))
             robotTypeName = "";
-        robotTypeName = RdJson::getString("robotType", "", _robotConfigs[n]);
+        robotTypeName = RdJson::getString("robotConfig/robotType", "", _robotConfigs[n]);
     }
 };
